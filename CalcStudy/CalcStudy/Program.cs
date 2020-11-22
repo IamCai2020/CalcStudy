e78c3e95c0b5c51189e02d83bb798e04d60eddf8
Iamhappy2018 <43491188+Iamhappy2018@users.noreply.github.com>
2020-11-22 22:43:32 +0800

    11/22内容

--- CalcStudy/CalcStudy/Program.cs
+++ CalcStudy/CalcStudy/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CaiEgg.Views;
+using System.Windows;
 
 namespace CalcStudy
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            CalculatorView view = new CalculatorView();
+            Application app = new Application();
+            app.Run(view);
         }
     }
 }
