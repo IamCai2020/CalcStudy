a200babdabb601e2c752a6c8e1f697601b6bf5e2
Cai <china_caizg@hotmail.com>
2020-11-26 17:22:57 +0800

    11/26

---
+++ CalcStudy/CaiCal.MathPro/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaiCal.MathPro
+{
+    public static class StringExtensions
+    {
+        public static string RemoveSpace(this string self) => self.Replace(" ", string.Empty);
+    }
+}
--- CalcStudy/CaiCal.MathPro/Token.Static.cs
+++ CalcStudy/CaiCal.MathPro/Token.Static.cs
@@ -85,5 +85,63 @@ namespace CaiCal.MathPro
                 }
             }
         }
+
+        public override string ToString()
+        {
+            return NormalizeString;
+        }
+
+        public static List<Token> Tokenize(string inp)
+        {
+            var ret = new List<Token>();
+            string str = inp.RemoveSpace();
+            int i = 0;
+            int cnt = str.Length;
+            StringBuilder token = new StringBuilder();
+            char c;
+
+            while (i < cnt)
+            {
+                c = str[i];
+                token = new StringBuilder(c.ToString());
+                if (c.IsDigit())
+                {
+                    while (i + 1 < cnt && str[i + 1].IsDigit())
+                    {
+                        token.Append(str[i + 1]);
+                        i += 1;
+                    }
+                }else if (c.IsLetter())
+                {
+                    while (i + 1 < cnt && str[i + 1].IsLetter())
+                    {
+                        token.Append(str[i + 1]);
+                        i += 1;
+                    }
+                }else if (c.IsOperator())
+                {
+                    if (c == '-' && (i == 0 || i > 0 && str[i - 1].IsLeftBracket()))
+                    {
+                        while(i + 1 < cnt && str[i + 1].IsDigit())
+                        {
+                            token.Append(str[i + 1]);
+                            i += 1;
+                        }
+                    }
+                }else if (c.IsLeftBracket() || c.IsRightBracket())
+                {
+
+                }
+                else
+                {
+                    throw new ArgumentException($"token={c.ToString()}???");
+                }
+
+                i += 1;
+                ret.Add(new Token(token.ToString()));
+            }
+
+            return ret;
+        }
     }
 }
